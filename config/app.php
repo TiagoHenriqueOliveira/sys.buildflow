e228548f1048dbc182037e9696aca32e0b959145
Tiago Henrique de Oliveira <tiago_murtosa@yahoo.com.br>
2026-09-02 09:58:45 -0300

    Ajusta tamanho da foto na Descricao para 260x326

--- config/app.php
+++ config/app.php
@@ -17,7 +17,7 @@ return [
 
     'name' => env('APP_NAME', 'Laravel'),
 
-    'version' => env('APP_VERSION', '1.8.2+57'),
+    'version' => env('APP_VERSION', '1.8.3+58'),
 
     /*
     |--------------------------------------------------------------------------
